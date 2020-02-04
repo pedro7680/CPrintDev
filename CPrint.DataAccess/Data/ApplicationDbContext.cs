d521b69a28e5b0d69312e779ce20f494ee0cb987
Peter Hoben <ped1704@gmail.com>
2020-02-04 11:23:07 +0000

    First Model Categories Added to Db and migration complete

--- CPrint.DataAccess/Data/ApplicationDbContext.cs
+++ CPrint.DataAccess/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using CPrint.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,6 +12,10 @@ namespace CPrint.DataAccess.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-        }
+           
+    }
+
+        // add category to the db table 
+        public DbSet<Category> Categories { get; set; }
     }
 }
---
+++ CPrint.Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CPrint.Models
+{
+    // set class to public to allow access
+    public class Category
+    {
+        // primary key
+        [Key]
+        public int Id { get; set; }
+
+
+        [Display(Name="Category Name")]
+        // required as name cannot be null
+        [Required]
+        // max length of the text can be 50
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+    }
+}
