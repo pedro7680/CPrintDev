97c7da8e87e2add5c0ac533e14dbc9fff8b8c8cb
pedro7680 <ped1704@gmail.com>
2020-02-23 14:30:36 +0000

    amendments made and uploaded all

---
+++ CPrint.Models/FoilBusinessCard.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CPrint.Models
+{
+    class FoilBusinessCard
+    {
+        // model for a business card test
+        // build data model for a super class . 
+        public int Id { get; set; }
+
+        // size 5 options
+
+        // orientation 3 options
+
+        // folding  4 options 
+
+        // paper type 5 option
+
+        // weight 5 options
+
+        // colour 4 options
+
+        // lamination 7 option
+
+        // quantity rquired
+
+
+
+
+        // then get the quote. 
+    }
+}
